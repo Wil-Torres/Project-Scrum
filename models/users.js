38edbb5eb0a2e21127d3b554885fab35ab02faa2
Wilson Torres <wtorres@tc.tecun.net.gt>
2017-09-07 16:06:31 -0600

    Se agregaron los metodos upate y delete para usuarios

--- models/users.js
+++ models/users.js
@@ -47,5 +47,38 @@ userModel.insertUser = function(objeto, callback){
     });
   }
 }
+userModel.updateUser = function(objeto, callback){
+  if(connection){
+    let sql = 'UPDATE users SET username = ' + connection.escape(objeto.username) + ',' +
+    'email = ' + connection.escape(objeto.email) + 'where id = ' + objeto.id;
+
+    connection.query(sql, function(error, result){
+      if(error){
+        throw error;
+      }else{
+        callback(null, {'msg':"success"});
+      }
+    })
+  }
+}
+userModel.deleteUser = function(id, callback){
+  if(connection){
+    let sqlExist = 'SELECT * FROM users where id = ' + connection.escape(id);
+    connection.query(sqlExist, function(error, row){
+      if(row){
+        var sql = 'DELETE FROM users WHERE id = ' + connection.escape(id);
+        connection.query(sql, function(error, result){
+          if(error){
+            throw error
+          }else{
+            callback(null, {'msg':'Delete'})
+          };
+        });
+      }else{
+        callback(null, {'msg':'User Not exist'})
+      }
+    })
+  }
+};
 
 module.exports = userModel;
